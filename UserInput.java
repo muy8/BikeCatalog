60b51387d84e77f0afb59c348c55951616c5fe01
Mateo Uy <mateouy98@gmail.com>
2015-09-23 16:45:03 -0700

    goes with the  array list

--- UserInput.java
+++ UserInput.java
@@ -1,4 +1,36 @@
+import java.util.Scanner;
+
+
 public class UserInput
 {
+Scanner in = new Scanner(System.in);
+		String userInput = in.nextLine();
+
+		userInput = userInput.toLowerCase();
+		
+		String bikename = "bike1";
+		String bikename1 = "bike2";
+		String bikename2 = "bike3";
+		Object b1 = bikename;
+		Object b2 = bikename1;
+		Object b3 = bikename2;
+
+		userInput.bikeStats();
+
+
+		if (userInput.equals(bikename))
+		{
+			gBike1.bikeStats();
+		} 
+		else if (userInput.equals(bikename1))
+		{
+			gBike2.bikeStats();
+		} 
+		else if (userInput.equals(bikename2))
+		{
+			gBike3.bikeStats();
+		}
+		
+		
 
 }
